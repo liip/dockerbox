ea6fcf61d0b92c8c9e0874586d57a7f85172a303
Christian Stocker <me@chregu.tv>
2014-06-01 16:27:04 +0200

    don't repeat the same command many times

--- ssh2docker.sh
+++ ssh2docker.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function ssh_to_box() {
+   local command=$@
+   if [ -z "$command" ]; then
+     ssh -q -p $PORT -i  ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -t vagrant@127.0.0.1 "if [[ -d '$(pwd)' ]]; then cd '$(pwd)'; fi; bash"
+   else
+     ssh -q -p $PORT -i  ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -t vagrant@127.0.0.1 "if [[ -d '$(pwd)' ]]; then cd '$(pwd)'; fi; $command"
+   fi
+
+}
 
 # A POSIX variable
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
@@ -29,11 +38,7 @@ if [[ -e "/tmp/dockerboxid" ]]; then
    ID=$(cat /tmp/dockerboxid | cut -f 1 -d ":");
    PORT=$(cat /tmp/dockerboxid | cut -f 2 -d ":");
    if [ ! -z "$ID" -a ! -z "$PORT" ]; then
-       if [ -z "$command" ]; then
-          ssh -q -p $PORT -i  ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -t vagrant@127.0.0.1 "if [[ -d $(pwd) ]]; then cd $(pwd); fi; bash"
-       else
-          ssh -q -p $PORT -i  ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -t vagrant@127.0.0.1 "if [[ -d $(pwd) ]]; then cd $(pwd); fi; $command"
-       fi
+       ssh_to_box $command
        if [[ ! $? -eq 0 ]]; then
            cachedid_error=1
        fi
@@ -59,10 +64,6 @@ if [ -z "$ID" -o $cachedid_error -eq 1 ]; then
        PORT=$(vagrant ssh-config $ID | grep Port | awk '{print $2}')
        echo "New dockerbox id, write it and login"
        echo -n "$ID:$PORT" > /tmp/dockerboxid
-       if [ -z "$command" ]; then
-          ssh -p $PORT -i  ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -t vagrant@127.0.0.1 "if [[ -d $(pwd) ]]; then cd $(pwd); fi; bash"
-	   else
-          ssh -p $PORT -i  ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -t vagrant@127.0.0.1 "if [[ -d $(pwd) ]]; then cd $(pwd); fi; $command"
-       fi
+       ssh_to_box $command
     fi
 fi
